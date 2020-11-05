353a8b1b26ac79a75d008615e0ff509d7031efcc
AzureRathalos <1481612340@qq.com>
2020-11-05 15:24:32 +0800

    发牌逻辑2

--- src/userPanel.java
+++ src/userPanel.java
@@ -145,6 +145,8 @@ public class userPanel {
                     slider.grabFocus();
                     checkedpass = false;
                 }
+                else
+                    checkedpass = true;
             }
         });
         pInput.add(bfp);
